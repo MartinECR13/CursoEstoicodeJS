e1039f0e91be2f7679cde1cbca01b6ef395b70c8
MartinECR13 <hee182er@gmail.com>
2023-09-01 19:53:11 -0600

    Modificando gitignore

---
+++ Modulo1/1_TipoDeDatosYVariables/js/app.js
@@ -0,0 +1,16 @@
+//Constantes
+const year = 1990;
+
+//Mostramelo en consola
+console.log(year);
+
+//Tipos de Datos:
+
+//Strings o cadenas de texto
+
+let nombre = 'Martin';
+let edad = '36';
+
+console.log('Yo me llamo ' + nombre + ' y mi edad es '+ edad);
+
+
